5b76422a9c75060fe5c6d8466365aee0069669a5
Teddy Horvath <teddyhorvath@Teddys-MacBook-Pro.local>
2022-09-04 12:16:02 -0700

    generated DispoItem component

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const DispoItem = () =>
+{
+  return (
+    <View style={styles.dispoContainer}>
+      <Image source=
+             {{ 
+               uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/uber-eats/restaurant1.jpeg',
+              }}
+              style={styles.image}
+              />
+      <Text style={styles.title}>El Cabo Coffe Bar Tres De Mayo</Text>
+      <Text style={styles.subtitle}>1.40 15-30 minutes</Text>
+    </View>
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <DispoItem />
+      <DispoItem />
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -13,8 +30,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColr: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 10,
+  },
+  dispoContainer:
+  {
+    width: '100%',
+    marginVertical: 10,
+  },
+  image:
+  {
+    width: '100%',
+    aspectRatio: 5/3,
+    marginBottom: 5,
+    
+  },
+  title: 
+  {
+    fontSize: 16,
+    fontWeight: '500',
+    marginVertical: 5,
+  },
+  subtitle: 
+  {
+    color: 'gray',
   },
 });
