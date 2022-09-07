22ce05fce8c96d186538f1e32673de4344fab159
Teddy Horvath <teddyhorvath@Teddys-MacBook-Pro.local>
2022-09-06 20:48:24 -0700

    implement order list

--- src/components/OrderListItem/index.js
+++ src/components/OrderListItem/index.js
@@ -1,8 +1,20 @@
 import { View, Text, Image } from 'react-native'
 const OrderListItem = ({order}) => {
   return (
-    <View>
-       <Image source={{uri: order.Restaurant.image}} style={{width: 100, height: 100}}></Image>
+    <View style={{flexDirection: 'row', margin: 10, alignItems: 'center'}}>
+       <Image source={{uri: order.Restaurant.image}} style={{width: 75, height: 75, marginRight: 5,}}></Image>
+
+       <View>
+           <Text style={{fontWeight: '600', fontSize: 16}}>
+                {order.Restaurant.name}
+           </Text>
+           <Text style={{marginVertical: 5}}>
+                3 items $38.45
+           </Text>
+           <Text>
+               2 days ago &#8226; {order.status} 
+           </Text>
+       </View>
     </View>
   )
 }
